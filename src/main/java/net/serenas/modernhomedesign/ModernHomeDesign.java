1ed49f54c4895cfd0dc00510a528b9f7a4718c7c
Sunskimmer82 <92886026+Sunskimmer822@users.noreply.github.com>
2024-05-16 15:05:30 -0500

    Initial commit

---
+++ src/main/java/net/serenas/modernhomedesign/ModernHomeDesign.java
@@ -0,0 +1,29 @@
+package net.serenas.modernhomedesign;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModernHomeDesign implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("modern-home-design");
+
+	//TODO: add expansion screws to stop gravity blocks from falling
+	//TODO: add galvanized steel beams in various shapes, should be slightly smaller collisionbox than iron bars and should align with beams
+	//TODO: add eco friendly wood veneers, should allow being placed on galvanized steel beams to change
+	//TODO: add trapdoor-like merging foldable table desk things (when activated, should check blocks on each side for more of them and should flip all up at once)
+	//TODO: make merging folding tables fold up/down with multiple blocks from wall
+	//TODO: wash basins
+	//TODO: storage for out-of-season clothing
+	//TODO: 
+
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Ready to design modern homes~");
+	}
+}
